33356ad680e8323c7d36042240a55d26b9d910fd
Alec Chan <Alec Chan>
2016-05-13 22:18:54 -0700

    initial commit

---
+++ Server/Server.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+using Server;
+using System.Net.Sockets;
+using NetComm;
+namespace Server
+{
+    class myServer
+    {
+        NetComm.Host serv;
+        bool isRunning;
+        
+        public myServer()
+        {
+            serv = new Host(2020);
+            serv.onConnection += new NetComm.Host.onConnectionEventHandler(Server_onConnection);
+            serv.lostConnection += new NetComm.Host.lostConnectionEventHandler(Server_lostConnection);
+            serv.DataReceived += new NetComm.Host.DataReceivedEventHandler(Server_DataReceived);
+            //Speeding up the connection
+            serv.SendBufferSize = 800;
+            serv.ReceiveBufferSize = 100;
+            serv.NoDelay = true;
+            
+        }
+
+        private void start()
+        {
+
+
+            if (!isRunning)
+            {
+                serv.StartConnection();
+                Console.WriteLine("Server Started...");
+                isRunning = true;
+            }
+            else
+            {
+                Console.WriteLine("Server is already running...");
+            }
+
+
+        }
+        private void stop()
+        {
+            if (isRunning)
+            {
+                serv.CloseConnection();
+                Console.WriteLine("Server Stopped...");
+                isRunning = false;
+            }
+            else
+            {
+                Console.WriteLine("Server is not running...");
+            }
+
+            
+        }
+
+        private void exit()
+        {
+            stop();
+            Environment.Exit(0);
+        }
+        private void list()
+        {
+            Console.WriteLine("Currently Connected Users:");
+            foreach(string u in serv.Users)
+            {
+                Console.WriteLine(u);
+            }
+            
+        }
+
+        public string GetLocalIPAddress()
+        {
+            var host = System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("Local IP Address Not Found!");
+        }
+
+        static string ConvertBytesToString(byte[] bytes)
+        {
+            return ASCIIEncoding.ASCII.GetString(bytes);
+        }
+
+        static byte[] ConvertStringToBytes(string str)
+        {
+            return ASCIIEncoding.ASCII.GetBytes(str);
+        }
+
+        void Server_onConnection(string id)
+        {
+            Console.WriteLine(id + " connected!"
+            ); //Updates the log textbox when new user joined
+            serv.Brodcast(ConvertStringToBytes("SERVER: "+id + " connected!"));
+        }
+        void Server_lostConnection(string id)
+        {
+            Console.WriteLine(id + " disconnected!"
+            ); //Updates the log textbox when new user joined
+            serv.Brodcast(ConvertStringToBytes("SERVER: " + id + " disconnected!"));
+        }
+        void Server_DataReceived(string ID, byte[] Data)
+        {
+            Console.WriteLine(ID + ": " + ConvertBytesToString(Data));   //Updates the log when a new message arrived, 
+                                                                         //converting the Data bytes to a string
+            string newMsg = ID + ": " + ConvertBytesToString(Data);
+            say(newMsg);
+            
+        }
+
+        void say(string line)
+        {
+            serv.Brodcast(ConvertStringToBytes(line));
+            
+        }
+
+        static void Main(string[] args)
+        {
+            myServer s = new myServer();
+            string input;
+            Console.WriteLine("Welcome!\nCurrent Time: " + DateTime.Now.ToShortTimeString() + "\nLocal Machine IP Address: "+s.GetLocalIPAddress());
+            s.isRunning = false;
+            s.start();
+
+            do
+            {
+                Console.Write(">");
+                input = Console.ReadLine();
+                input.ToLower();
+                input.Trim();
+
+                if (input == "start")
+                {
+                    s.start();
+                }
+                else if(input=="list")
+                {
+                    s.list();
+                }
+                else if(input=="stop")
+                {
+                    s.stop();
+                }
+                else if (input.Split(' ')[0]=="say")
+                {
+                    s.say(input.TrimStart(new []{ 's','a','y',' '}));
+                }
+                else
+                {
+                    Console.WriteLine("Command not found!");
+                }
+                
+            } while (input != "exit");
+            s.exit();
+        }
+    }
+}
---
+++ mChat/MainWindow.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Text.RegularExpressions;
+using NetComm;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Media;
+
+namespace mChat
+{
+    enum entityType
+    {
+        SERVER,
+        SELF,
+        OTHER
+    }
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Client c;
+        static string name;
+        public MainWindow()
+        {
+            InitializeComponent();
+            c = new Client();
+            c.Connected += new NetComm.Client.ConnectedEventHandler(client_Connected);
+            c.Disconnected += new NetComm.Client.DisconnectedEventHandler(client_Disconnected);
+            c.DataReceived += new NetComm.Client.DataReceivedEventHandler(client_DataReceived);
+            SCG.General.MarkovNameGenerator rng = new SCG.General.MarkovNameGenerator(getnames(),5,4);
+            name = rng.NextName;
+            //Speeding up the connection
+            c.SendBufferSize = 800;
+            c.ReceiveBufferSize = 100;
+            c.NoDelay = true;
+            Closing += new System.ComponentModel.CancelEventHandler((sender, e) => OnWindowClosing(sender, e, c));
+
+        }
+
+        List<string> getnames()
+        {
+            StreamReader file = new StreamReader("C:\\Users\\Alec\\documents\\visual studio 2015\\Projects\\mChat\\mChat\\Names.txt");
+            
+            return new List<string> (file.ReadToEnd().Split(' '));
+        }
+        void OnWindowClosing(object sender,System.ComponentModel.CancelEventArgs e, Client f)
+        {
+            if (f.isConnected) f.Disconnect();
+        }
+        void client_Connected()
+        {
+            sendMsg("SERVER: Connected to host!", entityType.SERVER);
+        }
+        void client_Disconnected()
+        {
+            sendMsg("SERVER: Disconnected from host!",entityType.SERVER);
+        }
+        void client_DataReceived(byte[] Data, string ID)
+        {
+            
+            sendMsg(ConvertBytesToString(Data),typeConverter(ConvertBytesToString(Data).Split(':')[0]));
+            
+        }
+
+        static entityType typeConverter(string type)
+        {
+            if(type=="SERVER")
+            {
+                return entityType.SERVER;
+            }
+            if(type==name)
+            {
+                return entityType.SELF;
+            }
+            else
+            {
+                return entityType.OTHER;
+            }
+        }
+
+        static string ConvertBytesToString(byte[] bytes)
+        {
+            return ASCIIEncoding.ASCII.GetString(bytes);
+        }
+
+        static byte[] ConvertStringToBytes(string str)
+        {
+            return ASCIIEncoding.ASCII.GetBytes(str);
+        }
+
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text != "")
+            {
+                c.SendData(ConvertStringToBytes(textBox.Text));
+                //sendMsg(textBox.Text, true);
+                
+            }
+            
+        }
+
+
+
+        void sendMsg(string msg, entityType sender)
+        {
+            HorizontalAlignment align=HorizontalAlignment.Center;
+            string bg= "#9C27B0";
+
+            switch (sender)
+            {
+                case entityType.SELF:
+                    align = HorizontalAlignment.Left;
+                    bg = "#9C27B0";
+                    break;
+                case entityType.OTHER:
+                    align = HorizontalAlignment.Right;
+                    bg = "#2196F3";
+                    break;
+                case entityType.SERVER:
+                    align = HorizontalAlignment.Center;
+                    bg = "#D50000";
+                    break;
+
+
+            }
+            
+            var bc = new BrushConverter();
+            string newString = msg.Substring(msg.IndexOf(":") + 2);
+            if (newString.StartsWith("https://www.youtube.com") || newString.StartsWith("https://youtube.com"))
+            {
+                
+                string ur = Regex.Match(newString, @"(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&amp;]v=)|youtu\.be\/)([^""&amp;?\/ ]{11})").Groups[1].Value;
+
+                WebBrowser b = new WebBrowser();
+
+                b.HorizontalAlignment = HorizontalAlignment.Stretch;
+                b.VerticalAlignment = VerticalAlignment.Stretch;
+
+
+                
+                listView.Items.Add(new Label() { Content = b, HorizontalAlignment = align, Background= (Brush)bc.ConvertFrom(bg) });
+
+                b.Navigate("https://www.youtube.com/embed/" + ur + "?&html5=1");
+                textBox.Text = "";
+
+            }
+            else {
+                listView.Items.Add(new Label() { Content = msg, HorizontalAlignment=align, Background=(Brush) bc.ConvertFrom(bg)});
+                textBox.Text = "";
+            }
+            
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            if(c.isConnected)
+                c.Disconnect();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (!c.isConnected)
+                c.Connect("localhost", 2020, name);
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
